a4ae4e7e4347049528d7bb0b9f52c496c7f7b14b
Max Vassilev <maximvassilev01@gmail.com>
2024-12-17 10:07:57 +0200

    Change structure

--- orchestrator/orchestrator.py
+++ orchestrator/orchestrator.py
@@ -1,4 +1,4 @@
-from user_database.user_db import get_all_users_from_dynamodb
+from user_service.user_db import get_all_users_from_dynamodb
 from user_service.user_model import User
 
 def get_all_users() -> list[User]:
